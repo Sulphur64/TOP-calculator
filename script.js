367b36d521e3cda1543522fe3c52cda433f1e6cf
Sulphur64 <128920405+Sulphur64@users.noreply.github.com>
2023-05-10 19:55:20 +0200

    day 2 logic rewritten, optiminised
    to do : solve switch/case problem with logical operator

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ const screenLine2 = document.querySelector("div.line2")
 const screenLine3 = document.querySelector("div.line3")
 const buttons = document.querySelectorAll("button.keys")
 
-screenLine1.textContent = objCalculator.active;
-screenLine2.textContent = objCalculator.passive;
 
 const objCalculator = {
     active: [0], //refactor: operand1 is now the active value. the only modifiable one
     operator: null,
-    lastOperator: null,
-    passive:[0],
+    operatorPrev: null,
+    passive:'',
 };
+
+screenLine1.textContent = objCalculator.active;
+screenLine2.textContent = objCalculator.passive;
 
 inputReact()
 
@@ -20,38 +21,50 @@ function inputReact(){ //maps the input
         button.addEventListener("click", kismet, false);
 
         function kismet(){
-            switch (button.dataset.key.textContent) {
+            console.log(button.textContent)
+            switch (button.textContent) {
 
-                case (1||2||3||4||5||6||7||8||9||0):
-
+                case ('0'||'1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'):
+                    console.log(button.textContent)
                     scribe();
                     break;
 
                 case ('.'):
 
-                    objCalculator.active.includes('.') ? screenLine3="Þórr disapproves." : scribe();
+                    objCalculator.active.includes('.') ? screenLine3.textContent="Þórr disapproves." : scribe();
                     break;
 
-                case ("-" ||"+" ||"*" || "/" || "="):                  
-                    objCalculator.operator=button.dataset.key.textContent;
-                    //must add the operation to calc. if first press, calc with 0 at a second operand. must reset operand 2 and update new number to active=result
+                case ("-" || "+" || "*" || "/"):                  
+                    Kalk(objCalculator.passive, objCalculator.active, objCalculator.operator)
+                    objCalculator.active=[0];
+                    objCalculator.operator=null;
+                    break;
 
-                    
+                case ("="):                  
+                    if((objCalculator.operatorPrev=='/') && (objCalculator.active==0)){
+                        screenLine3.textContent="Þórr forbid you."
+                    }else{
+                        Kalk(objCalculator.passive, objCalculator.active, objCalculator.operatorPrev)
+                        objCalculator.active=[0];
+                        objCalculator.operator=null;
+                    };
                     break;
 
                 case ("C"):
-                    objCalculator.reset()
-                
-                    screenLine3.textContent="No Errors Detected"
+                    if(objCalculator.active.length>1){
+                        objCalculator.active.pop();
+                        screenLine1.textContent = objCalculator.active.join(''); //could add slice(1)to remove 0
+
+                    }else screenLine3.textContent="Þórr disapproves.";
                     break;
 
                 case ("DEL"):
+                    objCalculator.active= [0];
+                    objCalculator.operator= null;
+                    objCalculator.operatorPrev= null;
+                    objCalculator.passive='';
 
-                    if(objCalculator.active.length>1){
-                        objCalculator.active.pop();
-                        screenLine1.textContent = objCalculator.active.join();
-
-                    }else screenLine3="Þórr disapproves.";
+                    screenLine3.textContent="No Errors Detected"
                     break;
             
                 default:
@@ -60,7 +73,7 @@ function inputReact(){ //maps the input
 
             function scribe(){
                 objCalculator.active.push(button.textContent);
-                screenLine1.textContent = objCalculator.active.join();
+                screenLine1.textContent = objCalculator.active.join('');
             };
         };
     });
@@ -68,4 +81,41 @@ function inputReact(){ //maps the input
     document.addEventListener("keydown",(event)=>{ 
         document.querySelector(`button[data-key="${event.code}"]`).click();
     });
+};
+
+function Kalk(a,b,operator){
+    switch (operator) {
+        case "-":
+            b = (parseFloat(b) - a.join('')).toFixed(2);
+            screenLine2.textContent = b+operator;
+            break;
+
+        case "+":
+            b = (parseFloat(b) + a.join('')).toFixed(2);
+            screenLine2.textContent = b+operator;
+            break;
+
+        case "/":
+            if(a==!0){
+                b = (parseFloat(b) / a.join('')).toFixed(2);
+                screenLine2.textContent = b+operator;
+            } else {
+                b = (a.join('') / 1).toFixed(2);
+                screenLine2.textContent = b+operator;
+            };
+            break;
+
+        case "*":
+            if(a==!0){
+                b = (parseFloat(b) * a.join('')).toFixed(2);
+                screenLine2.textContent = b+operator;
+            } else {
+                b = (a.join('') * 1).toFixed(2);
+                screenLine2.textContent = b+operator;
+            };
+            break;
+    
+        default:
+            break;
+    }
 };
