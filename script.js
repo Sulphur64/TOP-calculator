dac8897694accaec94415b34d0e2a20acfc79bf5
Sulphur64 <128920405+Sulphur64@users.noreply.github.com>
2023-05-10 15:43:47 +0200

    day 2 refactored logic
    operand 1 become 'active'
    operand 2 become 'passive'
    
    inputs works only on active
    operator pass the value of active to passive.
    operator does the math directly, equal call for the last operator
    
    clear reset objcalc,
    delete remove the last char of active.

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
 const screenLine1 = document.querySelector("div.line1")
+const screenLine2 = document.querySelector("div.line2")
+const screenLine3 = document.querySelector("div.line3")
 const buttons = document.querySelectorAll("button.keys")
 
+screenLine1.textContent = objCalculator.active;
+screenLine2.textContent = objCalculator.passive;
+
 const objCalculator = {
-    operand1: [],
+    active: [0], //refactor: operand1 is now the active value. the only modifiable one
     operator: null,
-    operand2:[],
-    result: 0,
+    lastOperator: null,
+    passive:[0],
 };
 
-inputReg()
+inputReact()
 
-function inputReg(){
+function inputReact(){ //maps the input
     buttons.forEach((button)=>{
-        button.addEventListener("click", scribe, false);
-        button.addEventListener("onkeydown", scribe, false);
+        button.addEventListener("click", kismet, false);
 
-        function scribe(){
-            
-            if (objCalculator.operator === null){
-                objCalculator.operand1.push(button.textContent);
-                screenLine1.textContent= objCalculator.operand1.join('')
+        function kismet(){
+            switch (button.dataset.key.textContent) {
+
+                case (1||2||3||4||5||6||7||8||9||0):
+
+                    scribe();
+                    break;
+
+                case ('.'):
+
+                    objCalculator.active.includes('.') ? screenLine3="Þórr disapproves." : scribe();
+                    break;
+
+                case ("-" ||"+" ||"*" || "/" || "="):                  
+                    objCalculator.operator=button.dataset.key.textContent;
+                    //must add the operation to calc. if first press, calc with 0 at a second operand. must reset operand 2 and update new number to active=result
+
+                    
+                    break;
+
+                case ("C"):
+                    objCalculator.reset()
                 
+                    screenLine3.textContent="No Errors Detected"
+                    break;
+
+                case ("DEL"):
+
+                    if(objCalculator.active.length>1){
+                        objCalculator.active.pop();
+                        screenLine1.textContent = objCalculator.active.join();
+
+                    }else screenLine3="Þórr disapproves.";
+                    break;
+            
+                default:
+                    break;
             }
+
+            function scribe(){
+                objCalculator.active.push(button.textContent);
+                screenLine1.textContent = objCalculator.active.join();
+            };
         };
+    });
+
+    document.addEventListener("keydown",(event)=>{ 
+        document.querySelector(`button[data-key="${event.code}"]`).click();
     });
 };
